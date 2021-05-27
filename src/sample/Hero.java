b38cd9230214136fdfa2418e88180a398442339f
Paweł Nowak <pavlnowak@gmail.com>
2021-05-27 14:59:41 +0200

    Add hero local parameters

--- src/sample/Hero.java
+++ src/sample/Hero.java
@@ -6,16 +6,31 @@ import javafx.scene.image.ImageView;
 import java.util.Iterator;
 
 public class Hero extends ImageView {
-    Hero()
-    {
+    boolean goNorth, goSouth, goEast, goWest;
+    double pos_x;
+    double pos_y;
+    double dx;
+    double dy;
+
+    Hero() {
         super("resources/Images/Thor.png");
+        goNorth = goSouth = goEast = goWest = false;
+        pos_x = getLayoutX();
+        pos_y = getLayoutY();
+    }
+    Hero(String path_to_image)
+    {
+        super(path_to_image);
+        goNorth = goSouth = goEast = goWest = false;
+        pos_x = getLayoutX();
+        pos_y = getLayoutY();
     }
     static void checkHitHero(Game game)
     {
         Iterator<Weapon> x=game.weaponsVillain.iterator();
         while(x.hasNext()){
             Node currentWeapon=x.next();
-            if (currentWeapon.getBoundsInParent().intersects(game.hero.getBoundsInParent())){
+            if (currentWeapon.getBoundsInParent().intersects(game.heroes.get(0).getBoundsInParent())){
                 game.lives--;
                 game.livesText.setText("Lives: " + game.lives);
                 Game.game.board.getChildren().remove(currentWeapon);
