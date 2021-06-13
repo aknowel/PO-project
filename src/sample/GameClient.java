e9e256b51555f92dfc007179f22342dd2c7e20f1
Paweł Nowak <paweln.nowak@student.uj.edu.pl>
2021-06-13 11:39:29 +0200

    Multi savepoint

--- src/sample/GameClient.java
+++ src/sample/GameClient.java
@@ -1,6 +1,10 @@
 package sample;
 
+import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class GameClient extends Game {
@@ -25,6 +29,31 @@ public class GameClient extends Game {
 
 
     public void play(Stage stage) {
-        while (true);
+        Hero.counter=0;
+        Counter.games();
+        Counter.thorGames();
+        Game.game.mode = mode;
+        scoreText = new Text(W / 2, 30, "Score: " + score);
+        for (Hero hero : heroes) {
+            board.getChildren().add(hero);
+        }
+        scoreText.setFont(new Font(30));
+        scoreText.relocate(W / 2 - scoreText.getBoundsInLocal().getWidth() / 2, 0);
+
+        for (int i = 0; i < heroes.size(); i += 1) {
+            hp_texts.put(heroes.get(i), new Text(170, 10, "HP: " + heroes.get(i).hp));
+            hp_texts.get(heroes.get(i)).setFont(new Font(30));
+            hp_texts.get(heroes.get(i)).setFill(Color.RED);
+            hp_texts.get(heroes.get(i)).relocate(10 + 150 * i, 0);
+            board.getChildren().add(hp_texts.get(heroes.get(i)));
+            board.getChildren().add(heroes.get(i).hpBar);
+        }
+
+        board.getChildren().add(scoreText);
+
+        Scene scene = new Scene(board, W, H, Color.POWDERBLUE);
+        stage.setScene(scene);
+        stage.setTitle("Ragnarok");
+        stage.show();
     }
 }
