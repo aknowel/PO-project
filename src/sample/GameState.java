3318f2f9d3c717877ca5a385e20054f1a93913c3
Paweł Nowak <paweln.nowak@student.uj.edu.pl>
2021-06-13 20:26:46 +0200

    Hero add/set working

--- src/sample/GameState.java
+++ src/sample/GameState.java
@@ -80,7 +80,7 @@ public class GameState {
             Background background = Background.newBackground(in.readInt());
             background.setLayoutX(in.readDouble());
             background.setLayoutY(in.readDouble());
-            //backgrounds.add(background);
+            backgrounds.add(background);
         }
     }
 
